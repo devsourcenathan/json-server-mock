7a71b8f06a4f5934b1f76487c6e625bd40d68559
Nathan <sadenachbinathan@gmail.com>
2023-09-27 09:36:39 +0100

    test

--- api/server.js
+++ api/server.js
@@ -5,13 +5,21 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+// const dataSeeder = require("./seeder")
+
+
 server.use(middlewares)
+
+// dataSeeder.seedData(router.db);
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter(
     routes
 ))
 server.use(router)
+
 server.listen(3000, () => {
+    // console.log(dataSeeder);
     console.log('JSON Server is running')
 })
 
