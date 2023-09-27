9fa45630d21bc2a4d94ddb9abb7673ebd7b71663
Nathan <sadenachbinathan@gmail.com>
2023-09-27 08:35:09 +0100

    updates routes

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+import routes from "./routes.json"
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(jsonServer.rewriter(
+    routes
+))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
