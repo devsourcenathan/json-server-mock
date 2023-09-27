6b33e5bf2f3521a32ac8e6ffb87e31210cc92628
Nathan <sadenachbinathan@gmail.com>
2023-09-27 21:51:25 +0100

    implement seeder

--- api/server.js
+++ api/server.js
@@ -1,26 +1,18 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-import routes from "./routes.json"
-// import seedData from "../seedData"
+const seedData = require("../seedData")
+const routes = require("./routes.json")
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
-
 server.use(middlewares)
 
-
-// Add this before server.use(router)
 server.use(jsonServer.rewriter(
     routes
 ))
 server.use(router)
 
-// seedData()
-
 server.listen(3000, () => {
-    // console.log(dataSeeder);
     console.log('JSON Server is running')
 })
 
--- seedData.js
+++ seedData.js
@@ -4,111 +4,100 @@ var faker = source.faker;
 
 const seedData = () => {
     // Générer des données factices pour chaque entité
-    const entities = Array.from({ length: 100 }).forEach(() => {
-        data.entities.push({
-            id: faker.string.uuid(),
-            fullName: faker.company.name(),
-            activity: faker.company.catchPhraseDescriptor(),
-            date: faker.date.recent(),
-            vat: faker.string.numeric(8),
-            registrationNumber: faker.finance.accountNumber(),
-            type: faker.string.fromCharacters(['fund', 'corporate']),
-            group: faker.string.fromCharacters(['Invoices', "Reports", "Scan", "Draff"]),
-            address: faker.location.country(),
-            currency: faker.finance.currencyName(),
-            jurisdiction: faker.location.countryCode(),
-            generalPartner: faker.company.name(),
-            assetsManager: faker.company.name(),
-            productType: faker.commerce.product(),
-            stats: {
-                cash_in: faker.finance.amount(),
-                cash_out: faker.finance.amount(),
-            },
-            contacts: Array.from({ length: 12 }).map(() => {
-                return {
-                    id: faker.string.uuid(),
-                    name: faker.person.fullName(),
-                    email: faker.internet.email(),
-                    phone: faker.phone.number(),
-                    address: faker.location.country()
-                }
-            }),
-            banks: Array.from({ length: 2 }).map(() => {
-                return {
-                    id: faker.string.uuid(),
-                    name: faker.finance.accountName(),
-                    email: faker.internet.email(),
-                    phone: faker.phone.number(),
-                    address: faker.location.country(),
-                }
-            }),
-            reports: Array.from({ length: 8 }).map(() => {
-                return {
-                    id: faker.string.uuid(),
-                    name: faker.finance.accountName(),
-                    email: faker.internet.email(),
-                    phone: faker.phone.number(),
-                    address: faker.location.country(),
-                }
-            }),
-        })
-    })
 
-    const currencies = Array.from({ length: 10 }).forEach(() => {
-        data.currencies.push({
-            id: faker.string.uuid(),
-            name: faker.finance.currencyCode(),
-            currentValue: faker.finance.amount(),
-            previousValue: faker.finance.amount(),
-            date: faker.date.recent(),
-        })
-    })
-
-    const documents = Array.from({ length: 100 }).forEach(() => {
-        data.documents.push({
-            id: faker.string.uuid(),
-            type: {
+    const entities = Array.from({ length: 100 }, () => ({
+        id: faker.string.uuid(),
+        fullName: faker.company.name(),
+        activity: faker.company.catchPhraseDescriptor(),
+        date: faker.date.recent(),
+        vat: faker.string.numeric(8),
+        registrationNumber: faker.finance.accountNumber(),
+        type: faker.string.fromCharacters(['fund', 'corporate']),
+        group: faker.string.fromCharacters(['Invoices', "Reports", "Scan", "Draff"]),
+        address: faker.location.country(),
+        currency: faker.finance.currencyName(),
+        jurisdiction: faker.location.countryCode(),
+        generalPartner: faker.company.name(),
+        assetsManager: faker.company.name(),
+        productType: faker.commerce.product(),
+        stats: {
+            cash_in: faker.finance.amount(),
+            cash_out: faker.finance.amount(),
+        },
+        contacts: Array.from({ length: 12 }).map(() => {
+            return {
                 id: faker.string.uuid(),
-                label: faker.string.fromCharacters(['pdf', 'word', 'excel']),
-                description: faker.lorem.sentence(),
-                created_at: faker.date.recent(),
-            },
-            transaction: faker.finance.transactionType(),
-            upload_by: faker.person.fullName(),
-            upload_date: faker.date.recent(),
-        })
+                name: faker.person.fullName(),
+                email: faker.internet.email(),
+                phone: faker.phone.number(),
+                address: faker.location.country()
+            }
+        }),
+        banks: Array.from({ length: 2 }).map(() => {
+            return {
+                id: faker.string.uuid(),
+                name: faker.finance.accountName(),
+                email: faker.internet.email(),
+                phone: faker.phone.number(),
+                address: faker.location.country(),
+            }
+        }),
+        reports: Array.from({ length: 8 }).map(() => {
+            return {
+                id: faker.string.uuid(),
+                name: faker.finance.accountName(),
+                email: faker.internet.email(),
+                phone: faker.phone.number(),
+                address: faker.location.country(),
+            }
+        }),
     })
-
-    const groups = Array.from({ length: 100 }).forEach(() => {
-        data.groups.push({
-            id: faker.string.uuid(),
-            name: faker.string.fromCharacters(['Invoices', "Reports", "Scan", "Draff"]),
-            currency: faker.finance.currencyName(),
-            jurisdiction: faker.location.countryCode(),
-            product: faker.commerce.productName(),
-            business: faker.commerce.department(),
-            date: faker.date.recent(),
-        })
-    })
-
-    const accounts = Array.from({ length: 3 }).forEach(() => {
-        data.accounts.push({
-            id: faker.string.uuid(),
-            iban: faker.finance.iban(),
-            balance: faker.finance.amount(),
-            last_transaction_date: faker.date.recent(),
-            date: faker.date.recent(),
-        })
-    })
-
-    const documentTypes = Array.from({ length: 6 }).forEach(() => {
-        data.documents_type.push({
+    )
+    const currencies = Array.from({ length: 10 }, () => ({
+        id: faker.string.uuid(),
+        name: faker.finance.currencyCode(),
+        currentValue: faker.finance.amount(),
+        previousValue: faker.finance.amount(),
+        date: faker.date.recent(),
+    }))
+    const documents = Array.from({ length: 100 }, () => ({
+        id: faker.string.uuid(),
+        type: {
             id: faker.string.uuid(),
             label: faker.string.fromCharacters(['pdf', 'word', 'excel']),
-            description: faker.lorem.text(),
-            created_at: faker.date.past(),
-        })
+            description: faker.lorem.sentence(),
+            created_at: faker.date.recent(),
+        },
+        transaction: faker.finance.transactionType(),
+        upload_by: faker.person.fullName(),
+        upload_date: faker.date.recent(),
     })
+    )
+    const groups = Array.from({ length: 100 }, () => ({
+        id: faker.string.uuid(),
+        name: faker.string.fromCharacters(['Invoices', "Reports", "Scan", "Draff"]),
+        currency: faker.finance.currencyName(),
+        jurisdiction: faker.location.countryCode(),
+        product: faker.commerce.productName(),
+        business: faker.commerce.department(),
+        date: faker.date.recent(),
+    })
+    )
+    const accounts = Array.from({ length: 3 }, () => ({
+        id: faker.string.uuid(),
+        iban: faker.finance.iban(),
+        balance: faker.finance.amount(),
+        last_transaction_date: faker.date.recent(),
+        date: faker.date.recent(),
+    })
+    )
+    const documentTypes = Array.from({ length: 6 }, () => ({
+        id: faker.string.uuid(),
+        label: faker.string.fromCharacters(['pdf', 'word', 'excel']),
+        description: faker.lorem.text(),
+        created_at: faker.date.past(),
+    })
+    )
 
     // Écrire les données factices dans le fichier `db.json`
     const fs = require('fs');
