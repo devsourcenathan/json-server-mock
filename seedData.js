9fb94de01bc93097d85223e4d7b08476fdb8fccb
Nathan <sadenachbinathan@gmail.com>
2023-10-18 10:28:21 +0100

    upgrade mock data

--- seedData.js
+++ seedData.js
@@ -3,8 +3,6 @@ const source = require('@faker-js/faker')
 var faker = source.faker;
 
 const seedData = () => {
-    // Générer des données factices pour chaque entité
-
     const entities = Array.from({ length: 12 }, () => ({
         id: faker.string.uuid(),
         fullName: faker.company.name(),
@@ -59,7 +57,8 @@ const seedData = () => {
         currentValue: faker.finance.amount(),
         previousValue: faker.finance.amount(),
         date: faker.date.recent(),
-    }))
+    })
+    )
     const documents = Array.from({ length: 12 }, () => ({
         id: faker.string.uuid(),
         type: {
@@ -99,16 +98,111 @@ const seedData = () => {
     })
     )
 
-    // Écrire les données factices dans le fichier `db.json`
+    const incomes = Array.from({ length: 3 }, () => ({
+        id: faker.string.uuid(),
+        code: faker.finance.routingNumber(),
+        description: faker.commerce.productAdjective(),
+        amount: faker.finance.amount(),
+        entity_type: faker.string.fromCharacters(['fund', 'corporate', 'bank'])
+    })
+    )
+
+    const reports = Array.from({ length: 2 }, () => ({
+        id: faker.string.uuid(),
+        created_at: faker.date.past(),
+        description: faker.commerce.productAdjective(),
+        user: {
+            id: faker.string.uuid(),
+            name: faker.person.fullName(),
+        },
+        assets: Array.from({ length: 3 }).map(() => {
+            return {
+                id: faker.string.uuid(),
+                year: faker.date.past().getDate(),
+                current_assets: {
+                    cash: faker.finance.amount(),
+                    account_receivable: faker.finance.amount(),
+                    inventory: faker.finance.amount(),
+                    short_term_investment: faker.finance.amount(),
+                    prepaid_experts: faker.finance.amount(),
+                },
+                fixed_assets: {
+                    long_term_investment: faker.finance.amount(),
+                    land: faker.finance.amount(),
+                    buildings: faker.finance.amount(),
+                    plant_and_equipment: faker.finance.amount(),
+                    furniture_and_fixture: faker.finance.amount(),
+                },
+                current_liabilities: {
+                    account_payable: faker.finance.amount(),
+                    short_term_note: faker.finance.amount(),
+                    current_portion: faker.finance.amount(),
+                    interet_payable: faker.finance.amount(),
+                    accrued_payroll: faker.finance.amount(),
+                    taxes_payable: faker.finance.amount(),
+                }
+            }
+        }),
+        entity_type: faker.string.fromCharacters(['fund', 'corporate', 'bank'])
+    })
+    )
+
+    const cash_flow = {
+        id: faker.string.uuid(),
+        data: Array.from({ length: 2 }).map(() => {
+            return {
+                id: faker.string.uuid(),
+                year: faker.date.recent().getDate(),
+                operatin_activities: {
+                    net_income: faker.finance.amount(),
+                    depreciation: faker.finance.amount(),
+                    deferred_taxes: faker.finance.amount(),
+                    account_receivable: faker.finance.amount(),
+                    others_assets_liabilities: faker.finance.amount(),
+                    inventory: faker.finance.amount(),
+                },
+                financing_activities: {
+                    cash_dividents_paid: faker.finance.amount(),
+                    repurchase: faker.finance.amount(),
+                    issuance: faker.finance.amount(),
+                    change_in_current_debt: faker.finance.amount(),
+                    change_in_ling_term_debt: faker.finance.amount(),
+                    others_funds: faker.finance.amount(),
+                },
+                net_cash_flow: {
+                    operating_cash_flow: faker.finance.amount(),
+                    capital: faker.finance.amount(),
+                },
+                free_cash_flow: faker.finance.amount(),
+
+            }
+        })
+    }
+
+    const reporting = Array.from({ length: 10 }, () => ({
+        id: faker.string.uuid(),
+        iban: faker.finance.iban(),
+        balance: faker.finance.amount(),
+        transaction_date: faker.date.past(),
+        entity_type: faker.string.fromCharacters(['fund', 'corporate', 'bank'])
+    })
+    )
+
     const fs = require('fs');
     const db = require('./db.json');
-    db.entities = entities;
-    db.currencies = currencies;
-    db.documents = documents;
-    db.groups = groups;
-    db.accounts = accounts;
-    db.documentTypes = documentTypes;
-    fs.writeFileSync('./db.json', JSON.stringify(db));
+    if (db.entities.length < 1) {
+        db.entities = entities;
+        db.currencies = currencies;
+        db.documents = documents;
+        db.groups = groups;
+        db.accounts = accounts;
+        db.documents_type = documentTypes;
+        db.incomes = incomes;
+        db.reports = reports;
+        db.cash_flow = cash_flow;
+        db.reporting = reporting;
+        fs.writeFileSync('./db.json', JSON.stringify(db));
+    }
 };
 
 seedData();
